2058c6e38b436ef040f1be15ed986d7c4ba00359
kotya <aelittaezugbaya@Sergeys-MacBook-Pro-2.local>
2018-03-03 17:54:53 +0200

    connect login functionalit to page

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,11 @@ import Map from './Map';
 import SideMenu from './SideMenu';
 import Login from './Login';
 import { Row, Col, Grid, Button } from 'react-bootstrap';
+import jwt_decode from 'jwt-decode';
 
 export default class App extends React.Component{
     constructor(props){
         super(props);
-
-        this.logIn = this.logIn.bind(this);
-    }
-
-    componentDidMount(){
-        console.log(window.location.hash);
         if(window.location.hash){
             const stateKey = 'spotify_auth_state';
             const params = this.getHashParams();
@@ -20,25 +15,15 @@ export default class App extends React.Component{
             const access_token = params.access_token
             const state = params.state
             const storedState = localStorage.getItem(stateKey);
-            if (access_token && (state == null || state !== storedState)) {
-              alert('There was an error during the authentication');
-            } else {
-              localStorage.removeItem(stateKey);
-                if (access_token) {
-                    window.fetch('https://api.spotify.com/v1/me', {
-                        method: 'GET',
-                        headers: {
-                        'Authorization': 'Bearer ' + access_token
-                        }
-                    })
-                    .then((response) => {return response.json()})
-                    .then((data) => {
-                        console.log(data)
-                    })
-                    
-                }
+            window.localStorage.accessToken = access_token;
+            this.state={
+                jwt: window.localStorage.accessToken
             }
         }
+        this.state={
+            jwt: window.localStorage.accessToken ? window.localStorage.accessToken: ''
+          }
+        
     }
     getHashParams() {
         const hashParams = {};
@@ -50,46 +35,30 @@ export default class App extends React.Component{
         return hashParams;
       }
 
-    generateRandomString(length) {
-        let text = '';
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < length; i++) {
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-      };
-
-    logIn(){
-        const stateKey1 = 'spotify_auth_state';
-        const client_id = '71247e62ff56404586d3398c81b57cf0'; // Your client id
-        const redirect_uri = 'http://localhost:8080/'; // Your redirect uri
-        const state = this.generateRandomString(16);
-        const stateKey = 'spotify_auth_state';
-        localStorage.setItem(stateKey1, state);
-        const scope = 'user-read-private user-read-email';
-        let url = 'https://accounts.spotify.com/authorize';
-        url += '?response_type=token';
-        url += '&client_id=' + encodeURIComponent(client_id);
-        url += '&scope=' + encodeURIComponent(scope);
-        url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-        url += '&state=' + encodeURIComponent(state);
-        window.location = url;
+    componentDidMount(){
         
     }
+
+  
     render(){
+        let {jwt}=this.state;
         return(
-            <Login/>
-            // <Grid fluid>
-            //     <Row>
-            //         <Col md={9}>
-            //             <Map/>
-            //         </Col>
-            //         <Col md={3}>
-            //             <SideMenu/>
-            //             <Button onClick={this.logIn}> Log in</Button>
-            //         </Col>
-            //     </Row>
-            // </Grid>
+            <div className='app'>
+                {jwt ?
+                <Grid fluid>
+                    <Row>
+                        <Col md={9}>
+                            <Map/>
+                        </Col>
+                        <Col md={3}>
+                            <SideMenu/>
+                            <Button onClick={this.logIn}> Log in</Button>
+                        </Col>
+                    </Row>
+                </Grid>
+                : <Login/>
+                }
+            </div>
         );
     }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,78 @@ import React from 'react';
 import { Row, Col, Grid, Button } from 'react-bootstrap';
 
 export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.logIn = this.logIn.bind(this);
+    }
+
+    componentDidMount(){
+        console.log(window.location.hash);
+        if(window.location.hash){
+            const stateKey = 'spotify_auth_state';
+            const params = this.getHashParams();
+
+            const access_token = params.access_token
+            const state = params.state
+            const storedState = localStorage.getItem(stateKey);
+            window.localStorage.accessToken = access_token;
+            if (access_token && (state == null || state !== storedState)) {
+              alert('There was an error during the authentication');
+            } else {
+              localStorage.removeItem(stateKey);
+                if (access_token) {
+                    window.fetch('https://api.spotify.com/v1/me', {
+                        method: 'GET',
+                        headers: {
+                        'Authorization': 'Bearer ' + access_token
+                        }
+                    })
+                    .then((response) => {return response.json()})
+                    .then((data) => {
+                        console.log(data)
+                    })
+                    
+                }
+            }
+        }
+    }
+    getHashParams() {
+        const hashParams = {};
+        let e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while ( e = r.exec(q)) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+      }
+
+    generateRandomString(length) {
+        let text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+      };
+
+    logIn(){
+        const stateKey1 = 'spotify_auth_state';
+        const client_id = '71247e62ff56404586d3398c81b57cf0'; // Your client id
+        const redirect_uri = 'http://localhost:8080/'; // Your redirect uri
+        const state = this.generateRandomString(16);
+        const stateKey = 'spotify_auth_state';
+        localStorage.setItem(stateKey1, state);
+        const scope = 'user-read-private user-read-email';
+        let url = 'https://accounts.spotify.com/authorize';
+        url += '?response_type=token';
+        url += '&client_id=' + encodeURIComponent(client_id);
+        url += '&scope=' + encodeURIComponent(scope);
+        url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+        url += '&state=' + encodeURIComponent(state);
+        window.location = url;
+        
+    }
     render(){
         return(
             <Grid fluid>
@@ -16,9 +88,8 @@ export default class Login extends React.Component{
                             <img src="./src/Spotify_logo_with_text.svg.png" />
                         </span>
                             <div className="container-login100-form-btn">
-                                <button className="login100-form-btn">
+                                <button className="login100-form-btn" onClick={this.logIn}>
                                     Sign in with spotify
-                                    
                                 </button>
 					    </div>
                     </div>
