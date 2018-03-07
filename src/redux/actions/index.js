240a780cad63eb09c3e405b2afa6fc6ffd5f38e2
aelittaezugbaya <aelittae@gmail.com>
2018-03-07 19:04:49 +0200

    added prettier, created function that will run after all requests are done

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,7 @@
 //your actions here
 const actions = {
-  RECEIVE_GIGS: "RECEIVE_GIGS",
-  UPDATE_SETTINGS: "UPDATE_SETTINGS",
+  RECEIVE_GIGS: 'RECEIVE_GIGS',
+  UPDATE_SETTINGS: 'UPDATE_SETTINGS',
 };
 
 export default actions;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,29 +1,25 @@
-import actions from "../actions";
+import actions from '../actions';
 
 const initialState = {
   gigs: [],
   settings: {
     date: null,
-    range: 250,
+    range: 50,
     city: 'Helsinki',
     genres: [],
-  }
+  },
 };
 
 function gigsApp(state, action) {
-  if (typeof state === "undefined") {
+  if (typeof state === 'undefined') {
     return initialState;
   }
 
   switch (action.type) {
     case actions.RECEIVE_GIGS:
-      return Object.assign(
-        {},
-        state,
-        {
-          gigs: action.payload
-        }
-      )
+      return Object.assign({}, state, {
+        gigs: action.payload,
+      });
       break;
     case actions.UPDATE_SETTINGS:
       return {
@@ -31,8 +27,8 @@ function gigsApp(state, action) {
         settings: {
           ...state.settings,
           ...action.payload,
-        }
-      }
+        },
+      };
     default:
   }
   // For now, don't handle any actions
